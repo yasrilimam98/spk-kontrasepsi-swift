d5b209e8f14a46cfa288b149e29abb5b4ee38d57
Yasril Imam <gallerycreatifease@gmail.com>
2020-07-27 22:47:29 +0800

    Initial Commit

---
+++ spk-kontrasepsi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  spk-kontrasepsi
+//
+//  Created by Devfx on 27/07/20.
+//  Copyright © 2020 Devfx. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
